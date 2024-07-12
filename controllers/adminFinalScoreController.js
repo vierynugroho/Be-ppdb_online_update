498f9d3be194b2a789e4cf557b7a954f1df0dbe7
QIAF <qonitaafifah3@gmail.com>
2024-07-13 00:18:09 +0700

    update: add logic for update admin_report_score

--- controllers/adminFinalScoreController.js
+++ controllers/adminFinalScoreController.js
@@ -22,6 +22,7 @@ const createFinalScore = async (req, res, next) => {
         user_id,
       },
     });
+
     console.log(total_report);
     const average_score = (health_score + interview_score + total_report.total_report_score) / 2;
     const data = {
@@ -41,26 +42,90 @@ const createFinalScore = async (req, res, next) => {
     return next(new ApiError(err.message, 400));
   }
 };
-const updateFinalScore = async (req, res, next) => {
-  const { health_score, interview_score } = req.body;
-  const id = req.params.id;
+// const updateFinalScore = async (req, res, next) => {
+//   const { health_score, interview_score } = req.body;
+//   try {
+//     const total_report = await studentReportScores.findOne({
+//       where: {
+//         user_id,
+//       },
+//     });
+//     console.log(total_report)
 
+//     const id = req.params.id;
+//     const findData = await AdminStudentFinalScore.findOne({
+//       where: {
+//         id,
+//       },
+//     });
+
+//     if (!findData) {
+//       return next(new ApiError(`Data with id ${id} not found`, 404));
+//     }
+//     const average_score = (health_score + interview_score + total_report.total_report_score)/2;
+//       await AdminStudentFinalScore.update(
+//       {
+//         user_id,
+//         health_score,
+//         interview_score,
+//         average_final_score: average_score
+//       },
+//       {
+//         where: {
+//           id,
+//         },
+//       }
+//     );
+//     const updateData = await AdminStudentFinalScore.findOne({
+//       where: {
+//         id,
+//       },
+//     });
+//     console.log(updateData);
+//     return
+//     res.status(200).json({
+//       status: "Success",
+//       message: "Final Score successfully updated",
+//       requestAt: req.requestTime,
+//       data: updateData,
+//     });
+//   } catch (err) {
+//     return next(new ApiError(err.message, 400));
+//   }
+// };
+
+const updateFinalScore = async (req, res, next) => {
+  const { health_score, interview_score, user_id } = req.body;
   try {
+    const total_report = await studentReportScores.findOne({
+      where: {
+        user_id,
+      },
+    });
+
+    if (!total_report) {
+      return next(new ApiError(`Total report with user_id ${user_id} not found`, 404));
+    }
+
+    const id = req.params.id;
     const findData = await AdminStudentFinalScore.findOne({
       where: {
         id,
       },
     });
-    console.log(findData)
-    return
 
     if (!findData) {
       return next(new ApiError(`Data with id ${id} not found`, 404));
     }
+
+    const average_score = (health_score + interview_score + total_report.total_report_score) / 2;
+
     await AdminStudentFinalScore.update(
       {
+        user_id,
         health_score,
         interview_score,
+        average_final_score: average_score,
       },
       {
         where: {
@@ -68,11 +133,13 @@ const updateFinalScore = async (req, res, next) => {
         },
       }
     );
+
     const updateData = await AdminStudentFinalScore.findOne({
       where: {
         id,
       },
     });
+
     res.status(200).json({
       status: "Success",
       message: "Final Score successfully updated",
