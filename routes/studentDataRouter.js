38f884da00a1a28742cb42bb1e05b676eb1bd363
QIAF <qonitaafifah3@gmail.com>
2024-07-14 22:26:23 +0700

    update: solve role and joi validation

--- routes/studentDataRouter.js
+++ routes/studentDataRouter.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
-const studentData = require("../controllers/studentDataController");
+const CheckRole = require ("../middlewares/role");
+const Authenticate = require("../middlewares/authentication");
+const Validator = require("../middlewares/validator");
+const {getStudentData, getStudentDataById, createStudentData, updateStudentData, deleteStudentData} = require ("../controllers/studentDataController");
+const {onlyAdminAndStudent} = require ("../utils/joiValidation")
 
-router.get("/", studentData.getStudentData);
-router.get("/:id",studentData.getStudentDataById);
-router.post("/", studentData.createStudentData);
-router.patch("/:id",studentData.updateStudentData);
-router.delete("/:id",studentData.deleteStudentData);
+router.get("/", Authenticate, CheckRole (onlyAdminAndStudent), getStudentData);
+router.get("/:id", Authenticate, CheckRole (onlyAdminAndStudent), getStudentDataById);
+router.post("/create", Authenticate, CheckRole (onlyAdminAndStudent), createStudentData);
+router.patch("/update/:id", Authenticate, CheckRole (onlyAdminAndStudent), updateStudentData);
+router.delete("/delete/:id", Authenticate, CheckRole(onlyAdminAndStudent), deleteStudentData);
+
 
 module.exports = router;
+
+
